d7eb3fbea0891052439001be9eca2a70f31e0654
Yan Xu <xudaiyanzi@gmail.com>
2021-08-15 02:02:46 -0500

    add details

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -2,13 +2,15 @@
  * ensure all variables on this page match your project
  */
 
+// Done!!!
+
 export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: '', // the auth0 domain prefix
-    audience: '', // the audience set for the auth0 app
-    clientId: '', // the client id generated for the auth0 app
+    url: 'yxfs.us.auth0.com', // the auth0 domain prefix
+    audience: 'drinks', // the audience set for the auth0 app
+    clientId: 'JBb4EvqoD56og5GPs2I1f42FyPniQmHY', // the client id generated for the auth0 app
     callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
   }
 };
